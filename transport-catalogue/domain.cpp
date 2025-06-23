0b4e294393d08ca3c75f97b579d18033657bd3d2
maksim <m4xin4t4@gmail.com>
2025-06-23 12:02:15 +0500

    tiny_encription_algorithm_number added

--- transport-catalogue/domain.cpp
+++ transport-catalogue/domain.cpp
@@ -1,7 +1,9 @@
 #include "domain.h"
 
+static constexpr size_t tiny_encryption_algorithm_number = 0x9e3779b9;
+
 size_t transport::StopPairHasher::operator()(const std::pair<const Stop*, const Stop*>& pair_of_stops) const {
     auto hash1 = std::hash<const Stop*>{}(pair_of_stops.first);
     auto hash2 = std::hash<const Stop*>{}(pair_of_stops.second);
-    return hash1 ^ (hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2));
+    return hash1 ^ (hash2 + tiny_encryption_algorithm_number + (hash1 << 6) + (hash1 >> 2));
 }
